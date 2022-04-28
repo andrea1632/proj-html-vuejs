e011a41db9427cde7e34601470da177617fd3bf5
andrea1632 <andrea.bellucci10@libero.it>
2022-04-28 13:49:29 +0200

    added HeaderComp

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <HeaderComp/>
+  </div>
+</template>
+
+<script>
+import HeaderComp from './components/HeaderComp.vue'
+export default {
+  name: 'App',
+  components: {
+    HeaderComp
+  }
+}
+</script>
+
+<style lang="scss">
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
+  .w80auto{
+    width: 80%;
+    margin: auto;
+  }
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,208 @@
+<template>
+    <div>
+        <header>
+            <div class="bgBlue">
+                <div class="top w80auto">
+                <div>
+                    <i class="fa-solid fa-clock"></i>
+                    <span>
+                        Open Hours: Mon-Sat-9:00 - 18:00
+                    </span>
+                </div>
+                <div class="rightTop">
+                    <div>
+                        <i class="fa-solid fa-phone"></i>
+                        <span>
+                            +1(305) 1234-5678
+                        </span>                       
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-envelope"></i>
+                        <span>
+                            hello@example.com
+                        </span>
+                    </div>
+                    <div class="topIcons">
+                        <i class="fa-brands fa-facebook-f"></i>
+                        <i class="fa-brands fa-twitter mx"></i>
+                        <i class="fa-brands fa-linkedin-in"></i>
+                    </div>
+                </div>
+            </div>            
+            </div>
+            <div class="hero">
+              <div class="overlay">  
+                <div class="w80auto">
+                    <div class="topContentHero">
+                        <div class="nexgen">
+                            <span>
+                                nex
+                            </span>
+                            gen
+                        </div>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="#">home</a>
+                                </li>
+                                <li>
+                                    <a href="#">about</a>
+                                </li>
+                                <li>
+                                    <a href="#">projects</a>
+                                </li>
+                                <li>
+                                    <a href="#">process</a>
+                                </li>
+                                <li>
+                                    <a href="#">testimonials</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa-regular fa-user"></i></a>
+                                </li>
+                                <li>
+                                    <button class="btn">
+                                        get in touch
+                                    </button>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="mainContentHero">
+                        <h4>
+                            human resources
+                        </h4>
+                        <div class="productivity">
+                            Get More
+                            <div>
+                                Productivity
+                            </div>                                
+                        </div>
+                        <p>
+                            Planning, recruitment and selection process and performance evaluation of employees
+                        </p>
+                        <div>
+                            <button class="btn">get in touch</button><button class="btnRead">read more</button>
+                        </div>
+                    </div>
+                </div>
+              </div>  
+            </div>
+        </header>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'HeaderComp',
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .top{
+      display: flex;
+      justify-content: space-between;
+      color: #C0C3D0;
+      padding: 15px 0;
+  }
+  .rightTop{
+      display: flex;
+      width: 30%;
+      justify-content: space-between;
+  }
+  .bgBlue{
+     background-color: #21333E;
+  }
+  .mx{
+      margin: 0 20px;
+  }
+  .hero{
+      background-image: url('./../assets/img/bg-4.jpg');
+  }
+  .overlay{
+      background-color: rgba(8, 9, 17, 0.466);
+  }
+  .nexgen{
+      text-transform: uppercase;
+      color: #C0C3D0;
+      font-weight: 600;
+      font-size: 1.2em;
+  }
+  .nexgen span{
+      background-color: #1F3D3D;
+      color: #058887;
+      padding: 10px;
+      border-top-left-radius: 20px;
+      border-bottom-left-radius: 20px;    
+  }
+  nav ul{
+      list-style: none;
+      display: flex;
+      align-items: center;
+  }
+  ul li a i{
+      margin-right: 10px;
+  }
+  ul li a{
+      text-transform: uppercase;
+      text-decoration: none;
+      color: white;
+      margin: 0 20px;
+  }
+  ul li a:hover{
+      color: #00A6A6;
+  }
+  .btn{
+      padding: 10px 20px;
+      text-transform: uppercase;
+      background-color: #058283;
+      color: white;
+      font-weight: 700;
+      border-style: none;
+      border-radius: 5px;
+      cursor: pointer;
+  }
+  .topContentHero{
+      display: flex;
+      justify-content: space-between;
+      padding-top: 30px;
+  }
+  .mainContentHero{
+      margin-top: 200px;
+      max-width: 60%;
+      h4{
+          text-transform: uppercase;
+          color: #00A6A6;
+      }
+      .productivity{
+          font-size: 5em;
+          font-weight: 900;
+          color: white;
+          div{
+            background-repeat: no-repeat;
+            background-image: linear-gradient(120deg, #135454 0%, #135454 100%);
+            background-size: 50% 0.4em;
+            background-position: 6px 70%;
+          }
+      }
+      p{
+          color: #929aa3;
+          font-size: 1.1em;
+          width: 50%;
+          padding: 30px 0;
+      }
+      .btnRead{
+      padding: 10px 20px;
+      background-color: transparent;
+      text-transform: uppercase;
+      border: 1px solid #058283;
+      color: white;
+      font-weight: 700;
+      border-radius: 5px;
+      cursor: pointer;
+      margin-left: 20px;
+      }
+      padding-bottom: 150px;
+  }
+</style>
